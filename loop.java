fdcb6949316e15abd6b0bf84332bcab0a37748ab
jyoti <jyotisaroja01@gmail.com>
2023-04-20 21:20:15 +0530

    loop code

---
+++ loop.java
@@ -0,0 +1,17 @@
+package WileyProject;
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<10;i++) {
+			
+			System.out.println("The value of i is:"+i);
+			for(int j=0;j<5;j++) {
+				System.out.println("The value of j is:"+i);
+			}
+		}
+
+	}
+
+}
