6de3ea025f08776f61d213b008b293c66405af2a
jyoti <jyotisaroja01@gmail.com>
2023-04-20 21:21:27 +0530

    whileloop

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+package WileyProject;
+import java.util.*;
+public class whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+         int i=1;
+         while(i<5) {
+        	 System.out.println(i);
+        	 i++;
+         }
+       
+	}
+
+}
