507b210f409ec3890ef2bbdd5cf50eb1d3d31be6
jyoti <jyotisaroja01@gmail.com>
2023-04-20 21:15:25 +0530

    first code

---
+++ code1.java
@@ -0,0 +1,18 @@
+package WileyProject;
+
+public class code1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+double $salary=100;
+System.out.println($salary);
+//typecasting
+int myInt = 10;
+double myDouble = myInt; 
+
+System.out.println(myInt);
+System.out.println(myDouble);
+	}
+
+}
