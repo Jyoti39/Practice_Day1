b86b006db55d6bd752a752c8a047f4e638f29294
jyoti <jyotisaroja01@gmail.com>
2023-04-20 21:20:48 +0530

    typecasting code

---
+++ typecasting.java
@@ -0,0 +1,16 @@
+package WileyProject;
+
+public class typecasting {
+
+	 public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   double myDouble = 9.78d;
+	   // byte myInt = (byte) myDouble; // Explicit casting: double to byte
+
+	    //System.out.println(myDouble);
+	    //System.out.println(myInt);
+	    char c='a';
+	    c++;
+	    System.out.println(c);
+	  }
+	}
