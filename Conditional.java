60b754050f81d144be1dfdf0392ca57088bb7321
jyoti <jyotisaroja01@gmail.com>
2023-04-20 21:18:32 +0530

    Conditional statement

---
+++ Conditional.java
@@ -0,0 +1,18 @@
+package WileyProject;
+
+public class Conditional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int age=21;
+        if(age>=18)
+        {
+        	System.out.println("you are ready to get marry");
+        	
+        }
+        else {
+        	System.out.println("You are not ready");
+        }
+	}
+
+}
